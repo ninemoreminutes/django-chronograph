3a763c250a5ffad7d5f2fd3b6d5ec513aa2d0295
Chris Church <chris@ninemoreminutes.com>
2014-01-06 04:51:22 -0500

    Updates to support Django 1.5.

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
 
 app_name = 'django-chronograph'
 
 setup(
     name=app_name,
-    version='0.1.11-1',
+    version='0.1.11-2',
     description='Django chronograph application.',
     author='Weston Nielson',
     author_email='wnielson@gmail.com',
